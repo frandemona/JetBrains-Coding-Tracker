5fa712558de45540dfbe07647d4e0f0448e356b7
GitHub Action <action@github.com>
2022-08-09 10:51:56 +0000

    Template cleanup

--- src/main/kotlin/com/github/frandemona/jetbrainscodingtracker/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/frandemona/jetbrainscodingtracker/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.frandemona.jetbrainscodingtracker.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.frandemona.jetbrainscodingtracker.MyBundle
 
 class MyApplicationService {
 
